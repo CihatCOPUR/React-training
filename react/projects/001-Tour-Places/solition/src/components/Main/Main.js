61ad82ad32700ea38e54e34c5aea9974315ca808
Cihat <copurcihat@gmail.com>
2022-10-11 21:50:24 +0300

    qq

--- react/projects/001-Tour-Places/solition/src/components/Main/Main.js
+++ react/projects/001-Tour-Places/solition/src/components/Main/Main.js
@@ -2,17 +2,19 @@ import "./Main.scss"
 
 import React from 'react'
 import {data} from "../../helper/data"
+import Card from "./Card"
 
 export const Main = () => {
     
   return (
    <div className="card-container">
     {
-        data.map((item)=>{
+        data.map((item,index)=>{
             return (
                 <div className="cards">
-                    <h1>{item.title}</h1>
-                    <img src={item.image} alt="" />
+                    
+                    <h1><Card {...item} key={index} /></h1>
+                    
                 </div>
             )
         })
